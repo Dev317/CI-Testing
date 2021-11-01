188d81ebb82078b3fd2c2ee991592df0f952c1aa
VU Quang Minh <vuquangminh2000317@gmail.com>
2021-11-01 17:06:26 +0800

    initial commit

---
+++ src/main/java/com/example/CI_testing/CiTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.CI_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CI_testing/CiTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CI_testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
